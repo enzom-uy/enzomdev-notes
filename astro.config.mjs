521920886e7af2873457fd82ae17ab0d44eb861c
enzom-uy <enzom.dev@gmail.com>
2023-01-07 15:32:00 -0300

    chore: integrate react, tailwind and vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), tailwind()],
+  output: "server",
+  adapter: vercel()
+});
