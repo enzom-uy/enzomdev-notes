ba20b437aaa8ab7634cb7f4ffaf2167839d8123f
enzom-uy <enzom.dev@gmail.com>
2023-01-13 19:34:10 -0300

    Revert "feat(posts): create sortByDate util function and sort all posts"
    This reverts commit be6652a4cf6693f32415a513f930e73dda06d677.

--- src/utils/sortByDate.ts
+++
@@ -1,7 +0,0 @@
-import type { MarkdownInstance } from 'astro'
-
-export const sortByDate = (posts: MarkdownInstance<Record<string, any>>[]) => {
-  return posts.sort(
-    (a, b) => Date.parse(b.frontmatter.Date) - Date.parse(a.frontmatter.date)
-  )
-}
